007dbed8e80285282e1809e782a6472ee9c7a9a0
Muhammad Bilal Liaquat <bilal@cognitivehealthintl.com>
2023-11-19 00:38:11 +0500

    reviewd error are resolved

--- db/migrate/20231118153157_create_posts.rb
+++ db/migrate/20231118153157_create_posts.rb
@@ -6,6 +6,7 @@ class CreatePosts < ActiveRecord::Migration[7.1]
       t.integer :comment_counter
       t.integer :likes_counter
       t.timestamps
+      t.references :author, foreign_key: { to_table: 'users' } 
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_11_18_153211) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
